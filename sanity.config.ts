7d8c3aeac4a7f6ace91dda4469af787b0235c483
Mohamed Mahmoud <96956614+sellwawy@users.noreply.github.com>
2024-12-05 10:02:06 +0000

    Start

---
+++ sanity.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'sanity'
+
+export default defineConfig({
+   projectId: 'lz1bac5t',
+   dataset: 'production',
+   title: 'E-commerce Studio',
+   apiVersion: '2023-12-05', // Update to today's date or the correct version.
+   plugins: [],
+})
